63aaeeb22e66a143c3dfeea46e48fa40e8e28d55
Stas Koshynskyi <stasony100@gmail.com>
2026-01-25 17:56:15 +0200

    fix address class: change isEmpty to isBlank

--- src/main/java/entity/Address.java
+++ src/main/java/entity/Address.java
@@ -2,13 +2,13 @@ package entity;
 
 public record Address (String city,String street, String buildNumber) {
     public Address {
-        if (city == null || city.isEmpty()) {
+        if (city == null || city.isBlank()) {
             throw new IllegalArgumentException("city can not be empty");
         }
-        if (street == null || street.isEmpty()) {
+        if (street == null || street.isBlank()) {
             throw new IllegalArgumentException("street can not be empty");
         }
-        if (buildNumber == null || buildNumber.isEmpty()) {
+        if (buildNumber == null || buildNumber.isBlank()) {
             throw new IllegalArgumentException("buildNumber can not be empty");
         }
 
