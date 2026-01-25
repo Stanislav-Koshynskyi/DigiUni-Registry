a540094ff7a72a34546bfdf22cf4e25d8d6c5b93
Stas Koshynskyi <stasony100@gmail.com>
2026-01-25 17:48:09 +0200

    add studentStatus enum

--- src/main/java/entity/StudentStatus.java
+++ src/main/java/entity/StudentStatus.java
@@ -1,4 +1,21 @@
 package entity;
 
 public enum StudentStatus {
+    STUDIES("навчається"),
+    EXPELLED("відрахований"),
+    ACADEMIC_LEAVE("академічна відпустка");
+
+    private final String nameUa;
+    StudentStatus(String nameUa) {
+        this.nameUa = nameUa;
+    }
+    public String getNameUa() {
+        return nameUa;
+    }
+
+    @Override
+    public String toString() {
+        return nameUa;
+    }
+
 }
