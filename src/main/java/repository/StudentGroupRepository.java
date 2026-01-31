4245003b7de5b2a6e2032521cb82dd289cb67403
Stanislav Koshynskyi <stasony10@gmail.com>
2026-01-31 22:39:54 +0200

    start develop student group repo

--- src/main/java/repository/StudentGroupRepository.java
+++ src/main/java/repository/StudentGroupRepository.java
@@ -1,16 +1,25 @@
 package repository;
 
+import entity.Department;
 import entity.StudentGroup;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class StudentGroupRepository extends AbstractRepositoryByLong<StudentGroup> {
     public StudentGroupRepository() {}
 
-    public
+    public List<StudentGroup> findByDepartment(Department department) {
+        Map<Long, StudentGroup> data = getData();
+        return data.values().stream().filter(sg -> sg.getDepartment.equals(department)).toList();
+    }
     /*
         private Long id;
     private String name;
     private Teacher headOfGroup;
     private Student groupLeader;
     private final HashSet<Student> students;
+    private Department department
      */
 }
