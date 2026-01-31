76bba4e0e179680bc8f1e9f702c0a63dcb110db9
Stanislav Koshynskyi <stasony10@gmail.com>
2026-01-31 22:14:15 +0200

    add repository (in process)

---
+++ src/main/java/repository/StudentGroupRepository.java
@@ -0,0 +1,16 @@
+package repository;
+
+import entity.StudentGroup;
+
+public class StudentGroupRepository extends AbstractRepositoryByLong<StudentGroup> {
+    public StudentGroupRepository() {}
+
+    public
+    /*
+        private Long id;
+    private String name;
+    private Teacher headOfGroup;
+    private Student groupLeader;
+    private final HashSet<Student> students;
+     */
+}
