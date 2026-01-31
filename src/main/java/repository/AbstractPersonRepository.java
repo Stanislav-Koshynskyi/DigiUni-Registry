88ae3b518b2a2a0e98b018f54070db7981bad545
Кошинський Станіслав Михайлович <stasony10@gmail.com>
2026-01-31 23:17:22 +0200

    Feature/repositories (#6)
    Implement In-Memory Repositories for Main Entities
    Added generic repository layer and specific implementations for :
    1)Student (based on Person repository), Teacher(based on Person repository),
    2)StudentGroup,
    3)University,
    4)Department,
    5)Faculty.
    Added generic abstract repository for person

---
+++ src/main/java/repository/AbstractPersonRepository.java
@@ -0,0 +1,67 @@
+package repository;
+
+import entity.Person;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+public abstract class AbstractPersonRepository <P extends Person> extends AbstractRepositoryByLong<P> {
+    public AbstractPersonRepository() {
+    }
+
+    public AbstractPersonRepository(long id) {
+        super(id);
+    }
+
+    public Optional<P> findByUniqueCode(String uniqueCode) {
+        Map<Long, P> data = getData();
+        return data.values().stream().filter(p -> p.getUniqueCode().equals(uniqueCode)).findAny();
+    }
+
+    public boolean existsByUniqueCode(String uniqueCode) {
+        Map<Long, P> data = getData();
+        return data.values().stream().anyMatch(p -> p.getUniqueCode().equals(uniqueCode));
+    }
+
+    public List<P> findByName(String name) {
+        Map<Long, P> data = getData();
+        return data.values().stream().filter(p -> p.getFullName().name().equalsIgnoreCase(name)).toList();
+    }
+
+    public List<P> findBySurname(String surname) {
+        Map<Long, P> data = getData();
+        return data.values().stream().filter(p -> p.getFullName().surname().equalsIgnoreCase(surname)).toList();
+    }
+
+    public List<P> findByPatronymic(String patronymic) {
+        Map<Long, P> data = getData();
+        return data.values().stream().filter(p -> p.getFullName().patronymic().equalsIgnoreCase(patronymic)).toList();
+    }
+
+    public Optional<P> findByEmail(String email) {
+        Map<Long, P> data = getData();
+        return data.values().stream().filter(p -> p.getContact().email().equals(email)).findFirst();
+    }
+
+    public boolean existsByEmail(String email) {
+        Map<Long, P> data = getData();
+        return data.values().stream().anyMatch(p -> p.getContact().email().equals(email));
+    }
+
+    public Optional<P> findByPhone(String phone) {
+        Map<Long, P> data = getData();
+        return data.values().stream().filter(p -> p.getContact().phone().equals(phone)).findFirst();
+    }
+
+    public boolean existsByPhone(String phone) {
+        Map<Long, P> data = getData();
+        return data.values().stream().anyMatch(p -> p.getContact().phone().equals(phone));
+    }
+
+    public List<P> findByBirthDate(LocalDate birthDate) {
+        Map<Long, P> data = getData();
+        return data.values().stream().filter(p -> p.getBirthDate().equals(birthDate)).toList();
+    }
+}
---
+++ src/main/java/repository/FacultyRepository.java
@@ -0,0 +1,67 @@
+package repository;
+
+import entity.Department;
+import entity.Faculty;
+import entity.Teacher;
+import entity.University;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+
+
+public class FacultyRepository extends AbstractRepositoryByLong<Faculty> {
+    public FacultyRepository() {}
+
+    public Optional<Faculty> findByUniqueCode(String uniqueCode) {
+        Map<Long, Faculty> data = getData();
+        return data.values().stream().filter(f -> f.getUniqueCode().equals(uniqueCode)).findFirst();
+    }
+
+    public boolean existsByUniqueCode(String uniqueCode) {
+        Map<Long, Faculty> data = getData();
+        return data.values().stream().anyMatch(f -> f.getUniqueCode().equals(uniqueCode));
+    }
+
+    public List<Faculty> findByName(String name) {
+        Map<Long, Faculty> data = getData();
+        return data.values().stream().filter(f -> f.getName().equalsIgnoreCase(name)).toList();
+    }
+
+
+    public List<Faculty> findByUniversity(University university) {
+        Map<Long, Faculty> data = getData();
+        return data.values().stream().filter(f -> f.getUniversity().equals(university)).toList();
+    }
+
+    public Optional<Faculty> findByDean(Teacher dean) {
+        Map<Long, Faculty> data = getData();
+        return data.values().stream().filter(f -> Objects.equals(f.getDean().orElse(null), dean)).findFirst();
+    }
+    public boolean existsByDean(Teacher dean) {
+        Map<Long, Faculty> data = getData();
+        return data.values().stream().anyMatch(f ->
+                Objects.equals(f.getDean().orElse(null), dean));
+    }
+
+    public Optional<Faculty> findByPhone(String phone) {
+        Map<Long, Faculty> data = getData();
+        return data.values().stream().filter(f -> f.getContact().phone().equals(phone)).findFirst();
+    }
+
+    public boolean existsByPhone(String phone) {
+        Map<Long, Faculty> data = getData();
+        return data.values().stream().anyMatch(f -> f.getContact().phone().equals(phone));
+    }
+
+    public Optional<Faculty> findByEmail(String email) {
+        Map<Long, Faculty> data = getData();
+        return data.values().stream().filter(f -> f.getContact().email().equals(email)).findFirst();
+    }
+
+    public boolean existsByEmail(String email) {
+        Map<Long, Faculty> data = getData();
+        return data.values().stream().anyMatch(f -> f.getContact().email().equals(email));
+    }
+}
