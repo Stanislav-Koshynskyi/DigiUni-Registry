cadb11f2c602b2041939fb1cfed6b5110d8a02e0
Кошинський Станіслав Михайлович <stasony10@gmail.com>
2026-02-10 22:29:32 +0200

    studentMenu now dont edit group when stundet transfer (#9)
    Co-authored-by: Stas Koshynskyi <stasony10@gmail.com>

--- src/main/java/ui/StudentMenu.java
+++ src/main/java/ui/StudentMenu.java
@@ -95,7 +95,6 @@ public class StudentMenu {
 
         Student student = new Student(uniqueCode, recordBook, fullName, birthDate, contact, form, status, yearOfAdmission, course, group);
         serviceStudent.create(student);
-        group.addStudent(student);
     }
 
     private void editStudent(Console console) {
