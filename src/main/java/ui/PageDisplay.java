5db408696f3225debe69fe5687f5137b0c48cc32
Stanislav Koshynskyi <stasony10@gmail.com>
2026-02-20 22:45:30 +0200

    add pageDisplay

---
+++ src/main/java/ui/PageDisplay.java
@@ -0,0 +1,46 @@
+package ui;
+
+import entity.Role;
+import entity.User;
+import util.MethodFilter;
+import util.Reader;
+
+import java.io.Console;
+import java.util.List;
+import java.util.Stack;
+
+public class PageDisplay {
+    // потім це перенести в sessionInfo(клас де зарашній юзер і стан логіна)
+    User currentUser = new User(Role.ADMIN, "asd","asd");
+    private Console console;
+    Stack<Page> history = new Stack<>();
+    public PageDisplay(Console console) {
+        this.console = console;
+    }
+    public void start(Page startPage){
+        history.push(startPage);
+        display();
+    }
+    private void display() {
+        while (true) {
+            if (history.isEmpty()) break;
+            Page currentPage = history.peek();
+            List<MenuItem> itemToShow = MethodFilter.filterItems(currentPage, currentUser);
+            for (int i = 1; i <= itemToShow.size(); i++) {
+                System.out.println(i + " - " + itemToShow.get(i - 1));
+            }
+            System.out.println("0 - exit");
+            int choose;
+            while (true) {
+                choose = Reader.readInt(console, "choose :");
+                if (choose >= 0 && choose <= itemToShow.size()) break;
+            }
+            if (choose == 0)
+                history.pop();
+            else{
+                history.push(itemToShow.get(choose-1).execute());
+            }
+
+        }
+    }
+}
