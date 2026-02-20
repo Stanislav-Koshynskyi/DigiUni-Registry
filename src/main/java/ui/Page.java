09aa0ec4fe6384d50c9b80e0b7f3b0b2db00e1e3
Stanislav Koshynskyi <stasony10@gmail.com>
2026-02-20 18:52:47 +0200

    add method filter in util

--- src/main/java/ui/Page.java
+++ src/main/java/ui/Page.java
@@ -3,5 +3,6 @@ package ui;
 import java.util.List;
 
 public interface Page {
+    String getTitle();
     List<MenuItem> getMenuItems();
 }
---
+++ src/main/java/util/MethodFilter.java
@@ -0,0 +1,19 @@
+package util;
+
+import ui.MenuItem;
+import ui.Page;
+
+import java.util.ArrayList;
+import java.util.List;
+import entity.User;
+public class MethodFilter {
+    public static List<MenuItem> filterItems(Page page, User user){
+        List<MenuItem> allMenuItems = page.getMenuItems();
+        List<MenuItem> filteredMenuItems = new ArrayList<>();
+        for (MenuItem menuItem : allMenuItems) {
+            if (user.canDo(menuItem.getRightNeeded()))
+                filteredMenuItems.add(menuItem);
+        }
+        return filteredMenuItems;
+    }
+}
