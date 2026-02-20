03b8b0154afa421520019896f213e921c5c32ab3
Stas Koshynskyi <stasony100@gmail.com>
2026-02-20 11:01:43 +0200

    add user

---
+++ src/main/java/entity/User.java
@@ -0,0 +1,41 @@
+package entity;
+
+public class User {
+    Role role;
+    String password;
+    String login;
+    public User(Role role, String password, String login) {
+        setRole(role);
+        setPassword(password);
+        setLogin(login);
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        if (role == null) throw new IllegalArgumentException("Role cannot be null");
+        this.role = role;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        if (password == null || password.isBlank())
+            throw new IllegalArgumentException("Password cannot be null or blank");
+        this.password = password;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        if (login == null || login.isBlank())
+            throw new IllegalArgumentException("Login cannot be null or blank");
+        this.login = login;
+    }
+}
