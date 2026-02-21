a7061ae6b86a3032928e0cdbfd3921c8db6a3498
Stanislav Koshynskyi <stasony10@gmail.com>
2026-02-21 15:02:54 +0200

    add userPage in page builder

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import repository.*;
+import security.*;
 import service.*;
 import ui.ConsoleMenu;
 import ui.Page;
@@ -16,6 +17,7 @@ public class Main {
         StudentGroupRepository studentGroupRepository = new InMemoryStudentGroupRepository();
         TeacherRepository teacherRepository = new InMemoryTeacherRepository();
         UniversityRepository universityRepository = new InMemoryUniversityRepository();
+        UserRepository userRepository = new InMemoryUserRepository();
 
         ServiceDepartmentInterface serviceDepartmentInterface = new ServiceDepartment(departmentRepository);
         ServiceFacultyInterface serviceFacultyInterface = new ServiceFaculty(facultyRepository);
@@ -23,12 +25,19 @@ public class Main {
         ServiceTeacherInterface serviceTeacherInterface = new ServiceTeacher(teacherRepository);
         ServiceUniversityInterface serviceUniversityInterface = new ServiceUniversity(universityRepository);
         ServiceStudentGroupInterface serviceStudentGroupInterface = new ServiceStudentGroup(studentGroupRepository);
+        ServiceUserInterface serviceUserInterface = new ServiceUser(userRepository);
+
+        SessionInfo sessionInfo = new LocalSessionInfo();
+
+        PasswordCoder coder = new BCryptPasswordEncoder();
+        AuthService authService = new LocalAuthService(coder, serviceUserInterface, sessionInfo);
 
         PagerBuilder pagerBuilder = new PagerBuilder(serviceTeacherInterface, serviceStudentInterface, serviceStudentGroupInterface, serviceDepartmentInterface,
-                serviceFacultyInterface, serviceUniversityInterface);
+                serviceFacultyInterface, serviceUniversityInterface,
+                authService, serviceUserInterface);
         Console console = System.console();
         Page mainPage = new MainPage(console, pagerBuilder);
-        PageDisplay pageDisplay = new PageDisplay(console);
+        PageDisplay pageDisplay = new PageDisplay(console, authService);
         pageDisplay.start(mainPage);
 
     }
