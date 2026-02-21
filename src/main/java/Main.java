35b6b6e057f3845327fc317077673b2b7bed26b1
Stanislav Koshynskyi <stasony10@gmail.com>
2026-02-21 14:50:41 +0200

    edit main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,35 @@
+import repository.*;
+import service.*;
 import ui.ConsoleMenu;
+import ui.Page;
+import ui.PageDisplay;
+import ui.pages.MainPage;
+import util.PagerBuilder;
+
+import java.io.Console;
 
 public class Main {
     public static void main(String[] args) {
-        ConsoleMenu consoleMenu = new ConsoleMenu();
-        consoleMenu.main();
+        DepartmentRepository departmentRepository = new InMemoryDepartmentRepository();
+        FacultyRepository  facultyRepository = new InMemoryFacultyRepository();
+        StudentRepository studentRepository = new InMemoryStudentRepository();
+        StudentGroupRepository studentGroupRepository = new InMemoryStudentGroupRepository();
+        TeacherRepository teacherRepository = new InMemoryTeacherRepository();
+        UniversityRepository universityRepository = new InMemoryUniversityRepository();
+
+        ServiceDepartmentInterface serviceDepartmentInterface = new ServiceDepartment(departmentRepository);
+        ServiceFacultyInterface serviceFacultyInterface = new ServiceFaculty(facultyRepository);
+        ServiceStudentInterface serviceStudentInterface = new ServiceStudent(studentRepository);
+        ServiceTeacherInterface serviceTeacherInterface = new ServiceTeacher(teacherRepository);
+        ServiceUniversityInterface serviceUniversityInterface = new ServiceUniversity(universityRepository);
+        ServiceStudentGroupInterface serviceStudentGroupInterface = new ServiceStudentGroup(studentGroupRepository);
+
+        PagerBuilder pagerBuilder = new PagerBuilder(serviceTeacherInterface, serviceStudentInterface, serviceStudentGroupInterface, serviceDepartmentInterface,
+                serviceFacultyInterface, serviceUniversityInterface);
+        Console console = System.console();
+        Page mainPage = new MainPage(console, pagerBuilder);
+        PageDisplay pageDisplay = new PageDisplay(console);
+        pageDisplay.start(mainPage);
+
     }
 }
