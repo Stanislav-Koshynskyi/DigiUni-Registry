394a19b4c0003b4e16f0adac1174a9898216ae94
Кошинський Станіслав Михайлович <stasony10@gmail.com>
2026-03-08 18:58:53 +0200

    feat: department found page (#49)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,10 +5,7 @@ import repository.*;
 import security.*;
 import service.*;
 import ui.*;
-import ui.finders.FacultyFinder;
-import ui.finders.FacultyFinderInterface;
-import ui.finders.UniversityFinder;
-import ui.finders.UniversityFinderInterface;
+import ui.finders.*;
 import ui.pages.MainPage;
 import util.PagerBuilder;
 
@@ -51,13 +48,20 @@ public class Main {
 
         UniversityFinderInterface universityFinderInterface = new UniversityFinder(serviceUniversityInterface, inputReader);
         FacultyFinderInterface facultyFinder = new FacultyFinder(serviceFacultyInterface, inputReader, universityFinderInterface);
-
+        DepartmentFinderInterface departmentFinder = new DepartmentFinder(inputReader, universityFinderInterface,
+                facultyFinder, serviceDepartmentInterface);
+        StudentGroupFinderInterface studentGroupFinder = new StudentGroupFinder(inputReader,
+                universityFinderInterface, facultyFinder, departmentFinder, serviceStudentGroupInterface);
+        StudentFinderInterface studentFinder = new StudentFinder(inputReader, universityFinderInterface, facultyFinder,
+                departmentFinder,studentGroupFinder, serviceStudentInterface);
+        TeacherFinderInterface teacherFinder = new TeacherFinder(serviceTeacherInterface, inputReader);
 
         PagerBuilder pagerBuilder = new PagerBuilder(serviceTeacherInterface, serviceStudentInterface,
                 serviceStudentGroupInterface, serviceDepartmentInterface,
                 serviceFacultyInterface, serviceUniversityInterface,
                 authService, serviceUserInterface, inputReader,
-                universityFinderInterface,  facultyFinder);
+                universityFinderInterface,  facultyFinder, departmentFinder,
+                studentGroupFinder, studentFinder, teacherFinder);
 
         Page mainPage = new MainPage(inputReader, pagerBuilder);
         PageDisplay pageDisplay = new PageDisplay(authService, inputReader);
---
+++ src/main/java/ui/finders/TeacherFinderInterface.java
@@ -0,0 +1,4 @@
+package ui.finders;
+
+public interface TeacherFinderInterface {
+}
