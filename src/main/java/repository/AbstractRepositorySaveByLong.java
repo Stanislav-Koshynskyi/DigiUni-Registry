6f3aef7687370ef8b96437f51bbd54cc9fcd5e6a
Stanislav Koshynskyi <stasony10@gmail.com>
2026-04-22 00:29:40 +0300

    docs: add IO logger

--- src/main/java/repository/AbstractRepositorySaveByLong.java
+++ src/main/java/repository/AbstractRepositorySaveByLong.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import entity.Entity;
 import exception.StorageException;
+import lombok.extern.slf4j.Slf4j;
 import util.IdGenerator;
 
 import java.io.File;
@@ -15,7 +16,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 import java.util.HashMap;
-
+@Slf4j
 public abstract class AbstractRepositorySaveByLong<T extends Entity>
         extends AbstractRepositoryByLong<T> implements Stored {
     private final ObjectMapper objectMapper;
@@ -40,6 +41,7 @@ public abstract class AbstractRepositorySaveByLong<T extends Entity>
                 Files.createDirectories(file.getParent());
             }
         } catch (IOException e) {
+            log.error("Error of create directory : {}", e.getMessage(), e);
             throw new StorageException("Error of create directory: " + file.getParent(), e);
         }
         loadFromFile();
@@ -52,10 +54,13 @@ public abstract class AbstractRepositorySaveByLong<T extends Entity>
                     currentId, new HashMap<>(getData())
             );
             objectMapper.writeValue(tempPath.toFile(), repositoryWrapperForSave );
+            log.info("save to temp file: {}", tempPath.getFileName());
             Files.move(tempPath, file,
                     StandardCopyOption.REPLACE_EXISTING,
                     StandardCopyOption.ATOMIC_MOVE);
+            log.info("Saved file at: {}", file.getFileName());
         } catch (IOException e) {
+            log.error("Failed save to file, {}, {}",e.getMessage(), e);
             throw new StorageException("Error to write in file: " + file.getFileName(), e);
         }
     }
@@ -74,8 +79,10 @@ public abstract class AbstractRepositorySaveByLong<T extends Entity>
             getData().putAll(wrapper.getData());
 
             setCurrentId(wrapper.getCurrentId());
+            log.info("Loaded data from file: {}", file);
 
         } catch (IOException e) {
+            log.error("Failed to load file {}. Reason: {}", file.getFileName(), e.getMessage(), e);
             throw new StorageException("Error of read file: " + file.getFileName(), e);
         }
     }
