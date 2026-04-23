cd5cd94e24d67086b108f0209e62f3338668bb07
Stanislav Koshynskyi <stasony10@gmail.com>
2026-04-23 18:44:27 +0300

    chore: delete non usage exception DepartmentNotCreatedException

--- src/main/java/exception/DepartmentNotCreatedException.java
+++
@@ -1,7 +0,0 @@
-package exception;
-
-public class DepartmentNotCreatedException extends RuntimeException {
-    public DepartmentNotCreatedException(String message) {
-        super(message);
-    }
-}
